7f9c7589387b4211b4cea32c4e4f55ea30cb7871
Vimaljeet Singh <jeetvimal98singh@gmail.com>
2020-09-04 22:33:55 +0530

    JSX Practice

---
+++ JSX/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import FunCom from './components/FunCom';
+import Welcome from './components/ClassCom';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<FunCom/>*/}
+      {/*<Welcome/>*/}
+      <Hello/>
+    </div>
+  );
+}
+
+export default App;
