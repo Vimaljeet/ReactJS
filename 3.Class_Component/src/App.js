1e4269a2c2d8a77bbfb3b67bd06bd6b0125bbace
Vimaljeet Singh <jeetvimal98singh@gmail.com>
2020-09-04 22:48:00 +0530

    Class Component Learning

---
+++ 3.Class_Component/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import FunCom from './components/FunCom';
+import Welcome from './components/ClassCom';
+
+function App() {
+  return (
+    <div className="App">
+      <FunCom/>
+      <Welcome/>
+    </div>
+  );
+}
+
+export default App;
