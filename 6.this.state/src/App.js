d9902cb93e0a2a287625eab89e3f7cab52ec4f49
Vimaljeet Singh <jeetvimal98singh@gmail.com>
2020-09-05 17:14:53 +0530

    this.state Learning

---
+++ 6.this.state/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+//import logo from './logo.svg';
+import "./App.css";
+//import FunCom from './components/FunCom';
+import Message from "./components/Message";
+//import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <div className="App">
+      {/*<FunCom/>*/}
+      {/*<Welcome/>*/}
+      {/*<Hello name="Vaishali" heroName="Virat">
+        <p>This is children props</p>
+      </Hello>
+      <Hello name="Yash" heroName="Sonam">
+        <button>Action</button>
+      </Hello>
+      <Welcome name="Vaishali" heroName="Virat" />
+      <Welcome name="Yash" heroName="Sonam" />
+  <Welcome name="Vimaljeet" heroName="Depp" /> */}
+
+      <Message />
+    </div>
+  );
+}
+
+export default App;
