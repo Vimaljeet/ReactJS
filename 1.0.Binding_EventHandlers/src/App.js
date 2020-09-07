b117d355ad5f1441c6e1d8eaf166ccf8a72b16e7
Vimaljeet Singh <jeetvimal98singh@gmail.com>
2020-09-07 10:53:19 +0530

    Binding Event Handlers Learning

---
+++ 1.0.Binding_EventHandlers/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import logo from './logo.svg';
+import "./App.css";
+import EventBind from "./components/EventBind";
+
+function App() {
+  return (
+    <div className="App">
+      <EventBind />
+    </div>
+  );
+}
+
+export default App;
