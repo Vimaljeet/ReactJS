4dfc2aeefbe3a8c4de50bfc320fdb71b7bb0de31
Vimaljeet Singh <jeetvimal98singh@gmail.com>
2020-09-07 10:50:51 +0530

    Methods as props Learning

---
+++ 11.Methods_as_props/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import logo from './logo.svg';
+import "./App.css";
+//import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComponent />
+    </div>
+  );
+}
+
+export default App;
